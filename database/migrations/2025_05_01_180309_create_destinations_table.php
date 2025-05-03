158d47248c43051bfb70c600164e4de1f35bd51a
benikib <benikasu7@gmail.com>
2025-05-03 17:52:11 +0200

    fix correction des erreur sur destination

--- database/migrations/2025_05_01_180309_create_destinations_table.php
+++ database/migrations/2025_05_01_180309_create_destinations_table.php
@@ -13,8 +13,8 @@ return new class extends Migration {
         Schema::create('destinations', function (Blueprint $table) {
             $table->id();
             $table->string('adresse');
-            $table->fl('longitude');
-            $table->integer('latitude');
+            $table->float('longitude');
+            $table->float('latitude');
             $table->string('tel_destination')->nullable();
             $table->timestamps();
         });
