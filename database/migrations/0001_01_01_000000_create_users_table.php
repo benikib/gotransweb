f078300d39e59c1da7af6c2077217952919910b3
benikib <benikasu7@gmail.com>
2025-05-03 18:58:53 +0200

    fix nullable champs number_phone actuellement

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('number_phone');
+            $table->string('number_phone')->nullable();
 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
