89645034248b4ab212f5babf5df31350279e7367
gradi <gradinchaki08@gmail.com>
2025-05-08 17:00:08 +0100

    creation users

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,8 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'fistname',
-        'lastname',
+    
+        'name',
         'email',
         'password',
     ];
