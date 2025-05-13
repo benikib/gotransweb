5fb6f0d6869e49315ae3416c5ccd8d2d99d6b511
benikib <benikasu7@gmail.com>
2025-05-13 16:34:37 +0200

    correction dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,11 +34,8 @@ function getBadgeClass($status) {
     };
 }
 @endphp
-
-<<<<<<< HEAD
-
 @section('content')
-
+<div class="container-fluid py-4">
    <div class="container-fluid py-2">
       <div class="row">
         <div class="ms-3">
@@ -46,217 +43,11 @@ function getBadgeClass($status) {
   <p class="text-sm text-muted mb-0">
     Consultez les Livraisons
   </p>
-</div>
-
-        
-        
-       <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-         <div class="card">
-            <div class="card-header p-3 pb-2">
-      <div class="d-flex justify-content-between align-items-center">
-        <div>
-          <p class="text-sm text-capitalize text-secondary mb-1">Total livraisons</p>
-          <h4 class="mb-0">{{ $livraisonsThisWeek }}</h4>
-        </div>
-        <div class="icon icon-md icon-shape bg-gradient-dark shadow text-center border-radius-lg">
-          <i class="material-symbols-rounded text-white">local_shipping</i>
-        </div>
-    </div>
-
-    <hr class="horizontal dark my-0">
-
-    <div class="card-footer p-3">
-      <p class="mb-0 text-sm">
-        <span class="text-{{ $livraisonsPourcentage >= 0 ? 'success' : 'danger' }} font-weight-bold">
-          {{ $livraisonsPourcentage >= 0 ? '+' : '' }}{{ $livraisonsPourcentage }}%
-        </span>
-         à la semaine dernière
-      </p>
-    </div>
-  </div>
-        </div>
-<div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-  <div class="card">
-    <div class="card-header p-3 pb-2">
-      <div class="d-flex justify-content-between align-items-center">
-        <div>
-          <p class="text-sm text-capitalize text-secondary mb-1">Utilisateurs aujourd'hui</p>
-          <h4 class="mb-0">{{ $usersToday }}</h4>
-        </div>
-        <div class="icon icon-md icon-shape bg-gradient-dark shadow text-center border-radius-lg">
-          <i class="material-symbols-rounded text-white">person</i>
-        </div>
-      </div>
-    </div>
-
-    <hr class="horizontal dark my-0">
-
-    <div class="card-footer p-3">
-      <p class="mb-0 text-sm">
-        <span class="text-{{ $usersPourcentage >= 0 ? 'success' : 'danger' }} font-weight-bold">
-          {{ $usersPourcentage >= 0 ? '+' : '' }}{{ $usersPourcentage }}%
-        </span>
-        par rapport à hier
-      </p>
-    </div>
-  </div>
-</div>
-
-<!-- Total Véhicules -->
-<div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-  <div class="card">
-    <div class="card-header p-2 ps-3">
-      <div class="d-flex justify-content-between">
-        <div>
-          <p class="text-sm mb-0 text-capitalize">Véhicules</p>
-          <h4 class="mb-0">{{ count($vehicules) }}</h4>
-        </div>
-        <div class="icon icon-md icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-lg">
-          <i class="material-symbols-rounded opacity-10">leaderboard</i>
-        </div>
-      </div>
-    </div>
-    <hr class="dark horizontal my-0">
-    <div class="card-footer p-2 ps-3">
-      <p class="mb-0 text-sm">
-        <span class="text-danger font-weight-bolder">-2%</span>
-        par rapport à hier
-      </p>
-    </div>
-  </div>
-</div>
-
-<!-- Total Types de Véhicule -->
-<div class="col-xl-3 col-sm-6">
-  <div class="card">
-    <div class="card-header p-2 ps-3">
-      <div class="d-flex justify-content-between">
-        <div>
-          <p class="text-sm mb-0 text-capitalize">Types de véhicule</p>
-          <h4 class="mb-0">{{ count($typeVehicules) }}</h4>
-        </div>
-        <div class="icon icon-md icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-lg">
-          <i class="material-symbols-rounded opacity-10">weekend</i>
-        </div>
-      </div>
-    </div>
-    <hr class="dark horizontal my-0">
-    <div class="card-footer p-2 ps-3">
-      <p class="mb-0 text-sm">
-        <span class="text-success font-weight-bolder">+5%</span>
-        par rapport à hier
-      </p>
-    </div>
-  </div>
-</div>
-
-
-        {{-- statistique --}}
-     <div class="row">
-  {{-- Carte 1 : Livraisons cette semaine --}}
-  <div class="col-lg-4 col-md-6 mt-4 mb-4">
-    <div class="card">
-      <div class="card-body">
-        <h6 class="mb-0">Livraisons cette semaine</h6>
-        <p class="text-sm">Comparé à la semaine dernière</p>
-        <div class="pe-2">
-          <div class="chart">
-            <canvas id="chart-bars" class="chart-canvas" height="170"></canvas>
-          </div>
-        </div>
-        <hr class="dark horizontal">
-        <div class="d-flex">
-          <i class="material-symbols-rounded text-sm my-auto me-1">trending_up</i>
-          <p class="mb-0 text-sm">
-            <span class="font-weight-bolder">
-              {{ $livraisonsPourcentage >= 0 ? '+' : '' }}{{ $livraisonsPourcentage }}%
-            </span> par rapport à la semaine dernière
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  {{-- Carte 2 : Utilisateurs aujourd’hui --}}
-  <div class="col-lg-4 col-md-6 mt-4 mb-4">
-    <div class="card">
-      <div class="card-body">
-        <h6 class="mb-0">Utilisateurs aujourd’hui</h6>
-        <p class="text-sm">
-          (<span class="font-weight-bolder">
-            {{ $usersPourcentage >= 0 ? '+' : '' }}{{ $usersPourcentage }}%
-          </span>) comparé à hier
-        </p>
-        <div class="pe-2">
-          <div class="chart">
-            <canvas id="chart-line" class="chart-canvas" height="170"></canvas>
-          </div>
-        </div>
-        <hr class="dark horizontal">
-        <div class="d-flex">
-          <i class="material-symbols-rounded text-sm my-auto me-1">person</i>
-          <p class="mb-0 text-sm">Aujourd’hui : {{ $usersToday }} utilisateur(s)</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  {{-- Carte 3 : Affectations Livreurs --}}
-  <div class="col-lg-4 mt-4 mb-3">
-    <div class="card">
-      <div class="card-body">
-        <h6 class="mb-0">Livreurs affectés</h6>
-        <p class="text-sm">Nombre total d'affectations</p>
-        <div class="pe-2">
-          <div class="chart">
-            <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
-          </div>
-        </div>
-        <hr class="dark horizontal">
-        <div class="d-flex">
-          <i class="material-symbols-rounded text-sm my-auto me-1">local_shipping</i>
-          <p class="mb-0 text-sm">Total : {{ count($livreur_vehicules) }} affectation(s)</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-      {{-- fonctionnalite --}}
-
-      <div class="row mb-4 p-3">
-        <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
-          <div class="card">
-            <div class="card-header pb-0">
-              <div class="row">
-                <div class="col-lg-6 col-7">
-                  <h6>Livraison recentes </h6>
-                  <p class="text-sm mb-0">
-                    {{-- <i class="fa fa-check text-info" aria-hidden="true"></i> --}}
-                    {{-- <span class="font-weight-bold ms-1">30 done</span> this month --}}
-                  </p>
-                </div>
-                <div class="col-lg-6 col-5 my-auto text-end">
-                  <div class="dropdown float-lg-end pe-4">
-                    <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown" aria-expanded="false">
-                      <i class="fa fa-ellipsis-v text-secondary"></i>
-                    </a>
-                    <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Action</a></li>
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Another action</a></li>
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Something else here</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-=======
-<div class="container-fluid py-4">
-    <!-- Page Header with Breadcrumb -->
+</div> <!-- Page Header with Breadcrumb -->
     <div class="row mb-4">
         <div class="col-12">
             <div class="page-header min-height-300 border-radius-xl" style="background-image: url('https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80');>
                 <span class="mask bg-gradient-dark opacity-6"></span>
->>>>>>> 59fed05e233e415ab01618df9fcd0e79acbed78c
             </div>
             <div class="card card-body mx-3 mx-md-4 mt-n6">
                 <div class="d-flex justify-content-between align-items-center">
@@ -471,6 +262,7 @@ function getBadgeClass($status) {
                         <h6 class="mb-0">Livraisons récentes</h6>
                         <div>
                             <button class="btn btn-sm btn-outline-primary">
+                
                                 <i class="material-symbols-rounded">list_alt</i> Voir tout
                             </button>
                         </div>
