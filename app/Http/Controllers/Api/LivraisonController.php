dfd2f99f5f83a836aee4c15ad1df031ebe716c49
marien manima <mmarien008@gmail.com>
2025-05-14 16:20:10 +0100

    finition des api concernant livraison

--- app/Http/Controllers/Api/LivraisonController.php
+++ app/Http/Controllers/Api/LivraisonController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Livraison;
+use App\Models\Expedition;
+use App\Models\Destination;
 use App\Http\Resources\LivraisonResource;
+use App\Http\Resources\ExpeditionResource;
 use Illuminate\Http\Request;
 
 class LivraisonController extends Controller
 {
-    public function index($idClient)
+    public function index()
 
     {
        
@@ -35,38 +38,37 @@ class LivraisonController extends Controller
     public function store(Request $request)
     {
 
-        // $expedition =  Expedition::create([
-        //     'adresse'=> $request->input("adresse_expedition"),
-        //     'tel_expedition'=> $request->input("tel_expedition"),
-        //     'longitude'=> 456.77,
-        //     'latitude'=> 456.7
-        // ]);
+          $expedition =  Expedition::create([
+              'adresse'=> $request->input("adresse_expedition"),
+              'tel_expedition'=> $request->input("tel_expedition"),
+              'longitude'=> 456.77,
+              'latitude'=> 456.7
+          ]);
 
-        // $destination =  Destination::create([
-        //     'adresse'=> $request->input("adresse_expedition"),
-        //     'tel_expedition'=> $request->input("tel_expedition"),
-        //     'longitude'=> 458.7,
-        //     'latitude'=> 456.7
-        // ]);
+          $destination =  Destination::create([
+              'adresse'=> $request->input("adresse_destination"),
+              'tel_expedition'=> $request->input("tel_destination"),
+              'longitude'=> 458.7,
+              'latitude'=> 456.7
+          ]);
+        
 
-      
-        // $livraison = Livraison::create([
-        //     'date' => $request->input('date'),
-        //     'status' => $request->input('status'),
-        //     'code' => $request->input('code'),
-        //     'montant' => $request->input('montant'),
-        //     'client_expediteur_id' => $request->input('client_expediteur_id'),
-        //     'client_destinateur_id' => $request->input('client_destinateur_id'),
-        //     'destination_id' => $destination->id,
-        //     'expedition_id' => $expedition->id,
-        //     'moyen_transport' => $request->input('moyen_transport'),
-        //     'vehicule_id' => 0,
-        //     'kilo_total'=>$request->input('Kilo_total')
-        // ]);
+          $livraison = Livraison::create([
+              'date' => $request->input('date'),
+              'status' => $request->input('status'),
+              'code' => $request->input('code'),
+              'montant' => $request->input('montant'),
+              'client_expediteur_id' => $request->input('client_expediteur_id'),
+              'client_destinateur_id' => $request->input('client_destinateur_id'),
+              'destination_id' => $destination->id,
+              'expedition_id' => $expedition->id,
+              'moyen_transport' => $request->input('moyen_transport'),
+              'vehicule_id' => 0,
+              'kilo_total'=>$request->input('Kilo_total')
+          ]);
 
-        return $request->all();  
+         return  json_encode( $livraison );
 
-    
     }
 
     /**
@@ -87,16 +89,47 @@ class LivraisonController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        $livraison = Livraison::find($request->input('id'));
+     
+         $livraison->update([
+           
+            'montant' => $request->input('montant'),
+            'kilo_total' => $request->input('kilo_total'),
+            'status' => "en_cours",
+          
+        ]);
+
+        return response()->json(['message' => 'Livraison updated successfully']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function cancel( $id)
     {
-        //
+         $livraison = Livraison::find($id);
+     
+         $livraison->update([
+
+            'status' => "annulee",
+          
+        ]);
+
+        return response()->json(['message' => 'Livraison annulee successfully']);
+    }
+
+     public function finish(string $id)
+    {
+       $livraison = Livraison::find($id);
+     
+         $livraison->update([
+
+            'status' => "validee",
+          
+        ]);
+
+        return response()->json(['message' => 'Livraison ok ']);
     }
 }
